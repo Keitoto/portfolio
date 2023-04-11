c395d58f7769fb3fed43ee58147696c64f8d1c2e
Keitoto <keitasekihara@gmail.com>
2023-04-11 18:23:36 +0900

    ✨ Added styled components and set font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,18 @@
 import Layout from '@/components/layout/Layout';
+import GlobalStyle from '@/styles/globalStyles';
 import type { AppProps } from 'next/app';
+
+import { Roboto_Flex } from 'next/font/google';
+
+const roboto = Roboto_Flex({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <GlobalStyle />
+      <Layout className={roboto.className}>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
