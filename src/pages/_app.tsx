1c4b100d0826462a4e0ead095861b47fa22ad8cc
Keitoto <keitasekihara@gmail.com>
2023-04-14 17:19:58 +0900

    ✨ Made useDarkTheme and replaced existing logic

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,19 @@
 import Layout from '@/components/layout/Layout';
+import { useDarkMode } from '@/hooks/useDarkMode';
 import { GlobalStyle } from '@/styles/GlobalStyles';
 import { lightTheme, darkTheme } from '@/styles/Theme';
 import type { AppProps } from 'next/app';
 
 import { Roboto_Flex } from 'next/font/google';
-import { useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 
 const roboto = Roboto_Flex({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [theme, setTheme] = useState('dark');
-  const themeToggler = () => {
-    theme === 'light' ? setTheme('dark') : setTheme('light');
-  };
+  const [theme, themeToggler,mountedComponent] = useDarkMode();
 
+  if(!mountedComponent) return <div/>
+  
   return (
     <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
       <GlobalStyle />
