22e462efb3529d7ab0224eb53036014be34ac63d
Keitoto <keitasekihara@gmail.com>
2023-04-18 16:07:12 +0900

    ✨ Added Footer

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+  return <StyledFooter>
+    <small>&copy;2023 Keita Sekihara</small>
+  </StyledFooter>;
+};
 
-export default Footer
+export default Footer;
+
+const StyledFooter = styled.footer`
+  color: ${({ theme: { color } }) => color.text};
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
