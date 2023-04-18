5acfdbea10177af80e21b80b5d5391de035a62cb
Keitoto <keitasekihara@gmail.com>
2023-04-18 16:09:07 +0900

    💄Added styled UI components

--- src/components/common/StyledUI.tsx
+++ src/components/common/StyledUI.tsx
@@ -6,3 +6,20 @@ export const StyledButton = styled.a`
   color: ${({ theme: { color } }) => color.primary};
   font-weight: 600;
 `;
+
+export const StyledContainer = styled.div`
+  max-width: var(--container-width);
+  width: 100%;
+  margin: 0 auto;
+`;
+
+export const StyledSection = styled.section`
+  padding: 30px var(--space-side-section);
+`
+
+export const StyledHeading = styled.h2`
+  font-size: 30px;
+  font-weight: bold;
+  margin-bottom: 20px;
+  text-align: center;
+`
