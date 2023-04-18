990cd25f3e1cce9c7d34ddfd6aa98622d0b50453
Keitoto <keitasekihara@gmail.com>
2023-04-18 16:10:41 +0900

    ✨ Added example Hero

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import { StyledContainer } from '@/components/common/StyledUI';
+import React from 'react';
+import styled from 'styled-components';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <>
+      <Section>
+        <Container>
+          <Heading>
+            Hi, I'm <span>Keita Sekihara</span>,
+          </Heading>
+          <Lead>a React Front-end developer</Lead>
+        </Container>
+      </Section>
+      <Section>
+        <BigHeadingContainer>
+          <BigHeading>
+            Hi, I'm <span>Keita Sekihara</span>,<br />Front-End <br/> React.js
+            Developer
+          </BigHeading>
+        </BigHeadingContainer>
+      </Section>
+    </>
+  );
+};
 
-export default Hero
+export default Hero;
+
+const Section = styled.section`
+  width: 100vw;
+  height: calc(100vh - var(--header-height) * 2);
+  margin-bottom: var(--header-height);
+`;
+const Container = styled(StyledContainer)`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+const Heading = styled.h1`
+  font-size: 48px;
+  span {
+    color: ${({ theme: { color } }) => color.primary};
+  }
+`;
+const Lead = styled.p`
+  font-size: 48px;
+`;
+
+const BigHeadingContainer = styled(StyledContainer)`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const BigHeading = styled.h1`
+  font-size: 60px;
+  span {
+    color: ${({ theme: { color } }) => color.primary};
+  }
+`;
+
+// Hi, I'm Keita Sekihara, 
+// Front-End React.js 
+// Web Developer
